eb4df57af7b96ebca443636c004b3734afc8d260
Lucas Carvalho de Sousa <encarvlucas@hotmail.com>
2020-03-29 17:12:14 -0300

    Added rules to normalize answer in guessing game, ignoring special chars, etc

--- src/app/pipes/string-compare.pipe.ts
+++ src/app/pipes/string-compare.pipe.ts
@@ -9,11 +9,20 @@ export class StringComparePipe implements PipeTransform {
     transform(answer: string, guess: Guess): boolean {
         try {
             return guess.answers
-                .map(item => item.toLowerCase())
-                .includes(answer.toLowerCase());
+                .map(item => this.normalize(item))
+                .includes(this.normalize(answer));
         } catch {
             return false;
         }
     }
 
+    normalize(text: string): string {
+        return text
+            .toLowerCase() // Remove case sensitivity
+            .normalize("NFD") // Split special characters in regular characters plus accents
+            .replace(/[\u0300-\u036f]/g, "") // Remove accents
+            .replace(/^the\s/, "") // Remove ambigous article "The "
+            .replace(/[\s\-\_]/g, "") // Remove whitespaces, hifens and underscores
+            ;
+    }
 }
