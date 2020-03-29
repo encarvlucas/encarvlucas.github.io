77a86c3a2dc987bdc9530010c5a8ab7b8a88afdd
Lucas Carvalho de Sousa <encarvlucas@hotmail.com>
2020-03-29 11:31:57 -0300

    Testing path location strategy to fix direct link to route bug, 404 error when connecting to valid route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LocationStrategy, PathLocationStrategy } from "@angular/common";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -45,7 +46,7 @@ import { SchedulerComponent } from "./components/scheduler/scheduler.component";
         MatIconModule,
         HttpClientModule,
     ],
-    providers: [],
+    providers: [{ provide: LocationStrategy, useClass: PathLocationStrategy }],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
